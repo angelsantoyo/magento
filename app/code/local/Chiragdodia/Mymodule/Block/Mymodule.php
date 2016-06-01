43dc795362faf9b3e0e0d3f77b69224ed1330e3d
Angel Santoyo <angel@gaiadesign.com.mx>
2016-06-01 16:46:00 -0500

    Add custom concatct form

---
+++ app/code/local/Chiragdodia/Mymodule/Block/Mymodule.php
@@ -0,0 +1,8 @@
+<?php
+class Chiragdodia_Mymodule_Block_Mymodule extends Mage_Core_Block_Template
+{
+    public function myfunction()
+    {
+        return "Hello world using myFunction in tu content layout";
+    }
+}
---
+++ app/code/local/Chiragdodia/Mymodule/controllers/IndexController.php
@@ -0,0 +1,13 @@
+<?php
+class Chiragdodia_Mymodule_IndexController extends Mage_Core_Controller_Front_Action
+{
+    public function indexAction()
+    {
+        $this->loadLayout();
+        $this->renderLayout();
+    }
+    public function testAction()
+    {
+        echo "test action";
+    }
+}
